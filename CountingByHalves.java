6434570300d5d901fb7df9384db76273833d7cb0
OpuniFrimpongGh <opunikf@gmail.com>
2016-03-17 13:39:57 -0400

    CountingByHalves

---
+++ CountingByHalves.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_10518584;
+
+/**
+ *
+ * @author opunifrimponggh
+ */
+
+
+public class CountingByHalves
+{
+    public static void main(String [] args)
+    {
+        
+        
+        System.out.println("   x   ");
+        System.out.println("_________");
+        
+        for (float i=-10; i<=10; i+=0.5)
+        {
+            System.out.print(i + " \n");
+        }
+        
+    }
+}
