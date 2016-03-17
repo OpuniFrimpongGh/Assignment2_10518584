78fd95fcbc8640888acde5e6e3365677f76e3fce
OpuniFrimpongGh <opunikf@gmail.com>
2016-03-17 13:08:39 -0400

    XsAndYs

---
+++ XsAndYs.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_10518584;
+
+/**
+ *
+ * @author opunifrimponggh
+ */
+
+
+public class XsAndYs
+{
+    public static void main(String [] args)
+    {
+        
+        
+        System.out.println(" x                y");
+        System.out.println("----------------------");
+        
+        for (double i=-10; i<=10; i+=0.5)
+        {
+            System.out.print(i + "\t\t" + i*i +" \n");
+        }
+        
+    }
+}
