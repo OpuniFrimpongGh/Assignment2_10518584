6299bbfdadff90a6af47fdd2b6b3b57ded320d13
OpuniFrimpongGh <opunikf@gmail.com>
2016-03-17 13:12:00 -0400

    NoticingEvenNumbers

---
+++ NoticingEvenNumbers.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_10518584;
+
+/**
+ *
+ * @author opunifrimponggh
+ */
+public class NoticingEvenNumbers
+{
+    public static void main(String [] args)
+    {
+       
+        System.out.println("Count to: ");
+        
+        for (int i=1; i<=20; i++)
+        {
+            
+            if (i%2==0)
+            {
+                System.out.println(i +"<" );
+            }
+            else 
+            System.out.println(i );
+        }
+        
+    }
+}
