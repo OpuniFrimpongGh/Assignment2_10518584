255ed9f3b8ffed428a048892d79055a5aa141a98
OpuniFrimpongGh <opunikf@gmail.com>
2016-03-17 13:14:47 -0400

    CountingFor

---
+++ CountingFor.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_10518584;
+
+/**
+ *
+ * @author opunifrimponggh
+ */
+import java.util.Scanner;
+
+public class CountingFor
+{
+    public static void main( String[] args )
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println( "Type in a message, and I'll display it five times." );
+        System.out.print( "Message: " );
+        String message = keyboard.nextLine();
+
+        for ( int n = 2 ; n <= 10 ; n = n+2 )
+        {
+            System.out.println( n + ". " + message );
+        }
+
+    }
+}
+
+// 1) It adds one to the value of one as each cycle in the loop is completed, but when it's removed
+//    an unended sequence of message display appears
+
+// 2) It's the needed condition for the loop to start; when removed, the code doesn't run
