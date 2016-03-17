e5c5c5d451908c3ab4aa839930cddd0c5e17b6ce
OpuniFrimpongGh <opunikf@gmail.com>
2016-03-17 13:14:24 -0400

    ForLoopChallenge

---
+++ ForLoopChallenge.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_10518584;
+
+/**
+ *
+ * @author opunifrimponggh
+ */
+public class ForLoopChallenge
+{
+    public static void main(String [] args)
+    {
+       
+        System.out.println("I'm counting from 15 to 30 by 3s: ");
+        int n1= 15;
+        int n2=30;
+        int n3= 3;
+        
+        
+        for (int j=n1; j<=n2; j+=n3)
+        {
+            System.out.print(j + " ");
+        }
+        
+    }
+}
