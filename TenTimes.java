bfaf22162b618e2b66d67114b86ebb5a9d51c84e
OpuniFrimpongGh <opunikf@gmail.com>
2016-03-17 13:11:22 -0400

    TenTimes

---
+++ TenTimes.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_10518584;
+
+/**
+ *
+ * @author opunifrimponggh
+ */
+public class TenTimes
+{
+    public static void main(String[] args)
+    {
+        int n=1;
+        for (int i=0; i<=9; i++)
+        {
+            System.out.println(n + ". " + "Mr. Mitchell is cool.");
+            n++;
+        }
+    }
+}
