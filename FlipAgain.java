30c5a268f32238df58805e2c80345c545d68b504
OpuniFrimpongGh <opunikf@gmail.com>
2016-03-17 13:18:26 -0400

    FlipAgain

---
+++ FlipAgain.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_10518584;
+
+/**
+ *
+ * @author opunifrimponggh
+ */
+import java.util.Random;
+import java.util.Scanner;
+
+public class FlipAgain
+{
+	public static void main( String[] args )
+	{
+		Scanner keyboard = new Scanner(System.in);
+		Random rng = new Random();
+
+		String again;
+
+/*		while ( again.equals("y") )
+		{
+			int flip = rng.nextInt(2);
+			String coin;
+
+			if ( flip == 1 )
+				coin = "HEADS";
+			else
+				coin = "TAILS";
+
+			System.out.println( "You flip a coin and it is... " + coin );
+
+			System.out.print( "Would you like to flip again (y/n)? " );
+			again = keyboard.next();
+		}*/
+             do 
+             {
+			int flip = rng.nextInt(2);
+			String coin;
+
+			if ( flip == 1 )
+				coin = "HEADS";
+			else
+				coin = "TAILS";
+
+			System.out.println( "You flip a coin and it is... " + coin );
+
+			System.out.print( "Would you like to flip again (y/n)? " );
+			again = keyboard.next();
+		} 
+             while ( again.equals("y"));
+             
+	}
+}
+
+// 3) The progtram still works because it executes the statements in the do loop 
+//    before it comes to check whether you want to do it againbor not.
+
