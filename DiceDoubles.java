2996dbb6bed64ec6a31dfd7c966fcd0fe38d9c94
OpuniFrimpongGh <opunikf@gmail.com>
2016-03-17 13:39:04 -0400

    DiceDoubles

---
+++ DiceDoubles.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_10518584;
+
+/**
+ *
+ * @author opunifrimponggh
+ */
+import java.util.Random;
+public class DiceDoubles {
+    public static void main(String[] args){
+    Random r = new Random();
+    
+    int x= 1 + r.nextInt(6);
+    int y= 1 + r.nextInt(6);
+    int z= x+y; 
+    System.out.println("HERE COMES THE DICE");
+    System.out.println();
+    while(x!=y)
+    {
+        x= 1 + r.nextInt(6);
+        y= 1 + r.nextInt(6);
+        z=x+y;
+        System.out.println ("Roll #1: " + x);
+        System.out.println ("Roll #2: " + y);
+        System.out.println ("The total is " + z);
+        System.out.println();
+    }
+        System.out.println ("Roll #1: " + x);
+        System.out.println ("Roll #2: " + y);
+        System.out.println ("The total is " + z);
+        System.out.println();
+}
+}
